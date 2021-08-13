0d7ed37764e752f1d1bb3c424973d940995a710f
Sophie Pfister <sophie@fluxlabs.ch>
2021-08-13 16:23:39 +0200

    Adapting config-GUI / BackUp 13.08.

--- src/Config/Repository.php
+++ src/Config/Repository.php
@@ -76,7 +76,9 @@ final class Repository extends AbstractRepository
     protected function getFields() : array
     {
         return [
-            ConfigFormGUI::KEY_SOME => Config::TYPE_STRING
+            ConfigFormGUI::KEY_BASEURL => Config::TYPE_STRING,
+            ConfigFormGUI::KEY_ONLYOFFICE_URL => Config::TYPE_STRING,
+            ConfigFormGUI::KEY_ONLYOFFICE_SECRET => Config::TYPE_STRING
         ];
     }
 }
