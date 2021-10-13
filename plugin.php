5360c08e36c6a4662af5132f06edc0f1d6658063
Sophie Pfister <sophie@fluxlabs.ch>
2021-10-13 15:34:36 +0200

    dbupdate

--- plugin.php
+++ plugin.php
@@ -2,8 +2,8 @@
 
 require_once __DIR__ . "/vendor/srag/dic/src/PHPVersionChecker.php";
 $id = "xono";
-$version = "1.0.0";
+$version = "1.0.1";
 $ilias_min_version = "5.4.0";
 $ilias_max_version = "7.999";
-$responsible = "studer + raimann ag - Team Custom 1";
-$responsible_mail = "support-custom1@studer-raimann.ch";
+$responsible = "fluxlabs";
+$responsible_mail = "connect@fluxlabs.ch";
