92fdeb88e6365bb2c2fde553b2c79329058ca0ac
Sophie Pfister <sophie@fluxlabs.ch>
2021-10-18 14:31:39 +0200

    Version 1.1.0

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__ . "/vendor/srag/dic/src/PHPVersionChecker.php";
 $id = "xono";
-$version = "1.0.3";
+$version = "1.1.0";
 $ilias_min_version = "5.4.0";
 $ilias_max_version = "7.999";
 $responsible = "fluxlabs";
